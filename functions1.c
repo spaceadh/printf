52c871285c3de1da30aacb45a2f3676c8e155408
Muoti5 <135327507+Muoti5@users.noreply.github.com>
2023-07-24 22:25:27 +0300

    With a face like mine, I do better in print

--- functions1.c
+++ functions1.c
@@ -122,6 +122,7 @@ int print_hexa_upper(va_list types, char buffer[],int flags, int width, int prec
  * @precision: Precision specification
  * @size: Size specifier
  * @size: Size specification
+ *
  * Return: Number of chars printed
  */
 int print_hexa(va_list types, char map_to[], char buffer[],
